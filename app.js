4fe3b43580e0d45021d50d454dc793b4bd684857
Shaun Pelling <shaunpelling@JAYNEs-iMac.local>
2017-08-10 16:17:03 +0100

    lesson 4 code

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const titles = document.getElementsByClassName('title');
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(wmf);
 
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
+var books = document.querySelector('#book-list li .name');
+console.log(books);
 
-Array.from(titles).forEach(function(title){
-  console.log(title);
+books = document.querySelectorAll('#book-list li .name');
+console.log(books);
+
+Array.from(books).forEach(function(book){
+  console.log(book);
 });
