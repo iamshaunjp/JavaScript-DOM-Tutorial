66f2a2e08055d6dd453e1de969d08296f3952964
Shaun Pelling <shaunpelling@JAYNEs-iMac.local>
2017-08-10 16:11:59 +0100

    lesson 3 code

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
+const titles = document.getElementsByClassName('title');
 
-console.log(search, bookList);
+console.log(Array.isArray(titles));
+console.log(Array.isArray(Array.from(titles)));
+
+Array.from(titles).forEach(function(title){
+  console.log(title);
+});
