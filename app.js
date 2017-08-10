aa36d976e440be1bde8bb6e8e2722b1b5483f348
Shaun Pelling <shaunpelling@JAYNEs-iMac.local>
2017-08-10 16:23:07 +0100

    lesson 5 code

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
-
-var books = document.querySelector('#book-list li .name');
-console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
+const books = document.querySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function(book){
-  console.log(book);
+  book.textContent += ' (Book title)';
 });
+
+const bookList = document.querySelector('#book-list');
+bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML content</p>';
