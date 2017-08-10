5dc8cb24a0b2bd5371e0e697f0fedf158e6c95f8
Shaun Pelling <shaunpelling@JAYNEs-iMac.local>
2017-08-10 17:40:41 +0100

    lesson 10 code

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const list = document.querySelector('#book-list ul');
 
+// delete books
 list.addEventListener('click', (e) => {
   if(e.target.className == 'delete'){
     const li = e.target.parentElement;
-    console.log('child element to remove:', li);
-    console.log('parent element to remove child from:', li.parentElement);
     li.parentNode.removeChild(li);
   }
-});
-
-// prevent default behaviour
-
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
-  e.preventDefault();
-  console.log('Navigation to', e.target.textContent, 'was prevented');
 });
