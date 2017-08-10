98182a6ad829afe764fda2e70b5cbd53736a5fa4
Shaun Pelling <shaunpelling@JAYNEs-iMac.local>
2017-08-10 17:38:50 +0100

    refactor

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const list = document.querySelector('#book-list ul');
+const listItems = document.querySelectorAll('#book-list ul li');
 
-list.addEventListener('click', (e) => {
-  if(e.target.className == 'delete'){
+Array.from(listItems).forEach(function(item){
+  item.addEventListener('click', (e) => {
+
     const li = e.target.parentElement;
     console.log('child element to remove:', li);
     console.log('parent element to remove child from:', li.parentElement);
     li.parentNode.removeChild(li);
-  }
+
+  });
 });
 
 // prevent default behaviour
