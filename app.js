8a62ef3d10f51f9a80e96a323e3cbda637b4aa62
Shaun Pelling <shaunpelling@JAYNEs-iMac.local>
2017-08-10 17:23:25 +0100

    lesson 8 code

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 const bookList = document.querySelector('#book-list');
 
-console.log('book list parent element:', bookList.parentElement);
-console.log('book list parent node:', bookList.parentNode);
+console.log('#book-list next sibling:', bookList.nextSibling);
+console.log('#book-list next element sibling:', bookList.nextElementSibling);
+console.log('#book-list previous sibling:', bookList.previousSibling);
+console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 
-console.log('all node children:');
-Array.from(bookList.childNodes).forEach(function(node){
-  console.log(node);
-});
-
-console.log('all element children:');
-Array.from(bookList.children).forEach(function(node){
-  console.log(node);
-});
-
-const titles = bookList.querySelectorAll('.name');
-
-console.log('Book titles:');
-Array.from(titles).forEach(function(title){
-  console.log(title.textContent);
-});
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
