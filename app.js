9a8131cb7940cec7d967c0b510aa48147494e6df
iamshaunjp <shaunjp@shaun-pelling.com>
2017-08-11 10:00:04 +0100

    lesson 15 code

--- app.js
+++ app.js
@@ -33,3 +33,13 @@ addForm.addEventListener('submit', function(e){
   li.appendChild(deleteBtn);
   list.appendChild(li);
 });
+
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+  if(hideBox.checked){
+    list.style.display = "none";
+  } else {
+    list.style.display = "initial";
+  }
+});
