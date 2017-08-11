6e0ac0817e73c95991dd06c607c5402baf1ec4b0
iamshaunjp <shaunjp@shaun-pelling.com>
2017-08-11 09:45:52 +0100

    lesson 13 code

--- app.js
+++ app.js
@@ -24,9 +24,12 @@ addForm.addEventListener('submit', function(e){
   bookName.textContent = value;
   deleteBtn.textContent = 'delete';
 
+  // add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
   // append to DOM
   li.appendChild(bookName);
   li.appendChild(deleteBtn);
   list.appendChild(li);
-  //list.insertBefore(li, list.querySelector('li:first-child'));
 });
