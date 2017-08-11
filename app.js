e922591c94dd36c1e0989913566d1439e7d39c72
iamshaunjp <shaunjp@shaun-pelling.com>
2017-08-11 10:14:11 +0100

    lesson 17 code

--- app.js
+++ app.js
@@ -58,3 +58,19 @@ searchBar.addEventListener('keyup', (e) => {
     }
   });
 });
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', (e) => {
+  if(e.target.tagName == 'LI'){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    Array.from(panels).forEach((panel) => {
+      if(panel == targetPanel){
+        panel.classList.add('active');
+      }else{
+        panel.classList.remove('active');
+      }
+    });
+  }
+});
