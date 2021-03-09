dc220746ef195c58a3931237dc716eff1b8e615c
ZaDivan <ivansadovnikov44@gmail.com>
2021-03-09 15:43:27 +0200

    lesson 4

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
+// console.log(wmf);
 
 var books = document.querySelector('#book-list li .name');
-console.log(books);
+// console.log(books);
 
 books = document.querySelectorAll('#book-list li .name');
-console.log(books);
+//console.log(books);
 
 Array.from(books).forEach(function(book){
   console.log(book);
