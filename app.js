707bfbf1a22ed718db06f0dfc58be2cd99b7b431
Seongmoon Kim <tokyokim0226@naver.com>
2023-12-22 11:25:41 +0900

    lesson 10 done

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+// const list = document.querySelector('#book-list ul');
+
+// // delete books
+// list.addEventListener('click', (e) => {
+//   if(e.target.className == 'delete'){
+//     const li = e.target.parentElement;
+//     li.parentNode.removeChild(li);
+//   }
+// });
+
+
+
 const list = document.querySelector('#book-list ul');
 
-// delete books
-list.addEventListener('click', (e) => {
-  if(e.target.className == 'delete'){
-    const li = e.target.parentElement;
-    li.parentNode.removeChild(li);
-  }
-});
+//delete books
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li);
+    }
+})
