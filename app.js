05f42ff82e432729d9e9b37956fa326cd2111c66
Seongmoon Kim <tokyokim0226@naver.com>
2023-12-22 13:55:58 +0900

    lesson 11 done

--- app.js
+++ app.js
@@ -18,4 +18,12 @@ list.addEventListener('click', function(e){
         const li = e.target.parentElement;
         li.parentNode.removeChild(li);
     }
+});
+
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
 })
