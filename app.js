484f66539f891c75823ebd8d9c3b63983bd03c9b
Seongmoon Kim <tokyokim0226@naver.com>
2023-12-22 14:04:09 +0900

    lesson 12 done

--- app.js
+++ app.js
@@ -25,5 +25,22 @@ const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function (e) {
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value);
+    
+    //create elements
+    const li = document.createElement('li'); //not input inside the dom yet, only created
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //append to Document
+    li.appendChild(bookName); //order does matter here
+    li.appendChild(deleteBtn);
+
+    //append the list to the unorderd list (ul) in the dom
+    list.appendChild(li) //the ul was selected using queryselected and asigned to list
+
+    //add textcontent to the bookName & deletebutton
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    deleteBtn.className = 'delete'
 })
